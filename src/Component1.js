8ea96e51a22e8c3a8ceeb398dbded12afddd710d
leedong2moving <mibou@handong.ac.kr>
2024-11-08 16:05:31 +0900

    1108

--- src/Component1.js
+++ src/Component1.js
@@ -1,9 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Component1 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: 'Component1',
+    };
+  }
+
+  handleClick = () => {
+    this.setState({
+      text: this.state.text === 'Component1' ? 'You clicked the button!' : 'Component1',
+    });
+  };
+
   render() {
     return (
-      <div>Component1</div>
-    )
+      <div>
+        <p>{this.state.text}</p>
+        <button onClick={this.handleClick}>Click me</button>
+      </div>
+    );
   }
 }
